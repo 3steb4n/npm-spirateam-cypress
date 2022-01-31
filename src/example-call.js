3113dc4b99f487f0341c6cd50ca04b38dc3855f1
Juan Gomez <juangomez1947@outlook.com>
2022-01-31 14:34:09 -0500

    Se crea libreria para consumir en cypress v1.0.0

---
+++ src/example-call.js
@@ -0,0 +1,15 @@
+const callFunctionUpdateTestRun = require("../../npm-spirateam-cypress") 
+
+const request = {
+    user: {
+        username: "esteban.gomez",
+        apiKey: "{D59E9599-001F-4DA1-B561-62133F60AA35}"
+    },
+    idProject: 37,
+    idRelease: 1880,
+    idTestCase: 29619,
+    idTestSet: 1621,
+    testFallido: true
+}
+
+callFunctionUpdateTestRun( request );
